acf5ff21bf4be0799d0742e2599696e9290cd7a5
Umair Iftikhar <umair.umi@live.co.uk>
2024-08-31 19:43:54 +0500

    index.js

--- src/index.js
+++ src/index.js
@@ -11,8 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
-<script async src="https://eocampaign1.com/form/d0803940-6778-11ef-a4e9-0769b67109ff.js" data-form="d0803940-6778-11ef-a4e9-0769b67109ff"></script>
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
